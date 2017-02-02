b49dafd271001a9b674671bbfb546a24b7be1fb3
kczapla <k.czapla68@gmail.com>
2017-02-02 13:52:07 +0100

    simple templates

--- app/views.py
+++ app/views.py
@@ -6,5 +6,17 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day'
+        },
+        {
+            'author': {'nickname': 'Susane'},
+            'body': 'Wonderful'
+        }
+    ]
     return render_template('index.html',
+                           title='Home',
+                           posts=posts,
                            user=user)
